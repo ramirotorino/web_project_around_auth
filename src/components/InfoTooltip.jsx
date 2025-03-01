6816674f3d6de3112a5dcbb82f2023c8cfb4fc1e
ramirotorino <ramirotorino@hotmail.com>
2025-03-01 20:48:40 +1000

    Mejora de visualizacion popup

--- src/components/InfoTooltip.jsx
+++ src/components/InfoTooltip.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import successIcon from "../images/exito-icon.svg";
 import errorIcon from "../images/error-icon.svg";
+import CloseIcon from "../images/CloseIcon.svg";
 
-const InfoTooltip = ({ isSuccess, message, isOpen, onClose }) => {
+const InfoTooltip = ({ isSuccess, isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
     <div className="info-tooltip info-tooltip__opened">
       <div className="info-tooltip__container">
         <button className="info-tooltip__close-button" onClick={onClose}>
-          &times;
+          <img
+            src={CloseIcon}
+            alt="Cerrar popup"
+            className="info-tooltip__close-icon"
+          />
         </button>
 
         <img
@@ -18,7 +23,11 @@ const InfoTooltip = ({ isSuccess, message, isOpen, onClose }) => {
           className="info-tooltip__icon"
         />
 
-        <p className="info-tooltip__message">{String(message)}</p>
+        <p className="info-tooltip__message">
+          {isSuccess
+            ? "¡Correcto! Ya estás registrado."
+            : "Uy, algo salió mal. Por favor, inténtalo de nuevo."}
+        </p>
       </div>
     </div>
   );
