2a7a3e115fc8f5cb5840ec644d3f5fe24edc54a5
ramirotorino <ramirotorino@hotmail.com>
2025-03-01 21:49:29 +1000

    agregar estilos y clases a header para usuario

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useContext } from "react";
+
 import logo from "../../images/logo.svg";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Header = () => {
   const isAuthenticated = localStorage.getItem("token") !== null;
   const navigate = useNavigate();
+  const location = useLocation();
+  const { currentUser } = useContext(CurrentUserContext);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("currentUser");
     navigate("/signin");
   };
 
@@ -15,12 +20,19 @@ const Header = () => {
     <header className="header">
       <img src={logo} alt="Logo de Around The U.S." className="header__logo" />
       <nav className="header__nav">
-        {location.pathname === "/signin" ? (
-          <Link to="/signup" className="header__link">
+        {isAuthenticated && currentUser ? (
+          <div className="header__user-info">
+            <span className="header__email">{currentUser.email}</span>
+            <button className="header__logout" onClick={handleLogout}>
+              Cerrar sesión
+            </button>
+          </div>
+        ) : location.pathname === "/signin" ? (
+          <Link to="/signup" className="header__link header__link--white">
             Regístrate
           </Link>
         ) : location.pathname === "/signup" ? (
-          <Link to="/signin" className="header__link">
+          <Link to="/signin" className="header__link header__link--white">
             Iniciar sesión
           </Link>
         ) : null}
